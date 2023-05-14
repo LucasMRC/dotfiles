17779b0f431f41c1efd3be5c022c787213de3807
LucasMRC <lucas.reynacordoba22@gmail.com>
2023-05-14 10:33:27 +0200

    sets up ssh-agent to authenticate to github

--- fish/config.fish
+++ fish/config.fish
@@ -14,3 +14,9 @@ alias standing-setup='sh /home/lucas/.screenlayout/standing-setup.sh'
 if [ -f /usr/bin/neofetch ]
     neofetch | lolcat
 end
+
+# Set up ssh-agent with GH key
+if [ -z "$SSH_AUTH_SOCK" ]
+    eval "$(ssh-agent -c)" &>/dev/null
+    ssh-add -q ~/.ssh/id_ed25519_GH
+end
