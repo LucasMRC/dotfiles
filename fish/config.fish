30c65c14fedfc13db2a9bb8608f1598580d54650
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-02-10 07:27:10 +0100

    moves config files to .dotconfig folder

--- fish/config.fish
+++ fish/config.fish
@@ -5,31 +5,13 @@ end
 # Supresses fish's intro message
 set fish_greeting
 
-# Syntax:
-# To just rerun your last command, simply type '!!'
-# '!! sudo' will prepend sudo to your most recent command
-# Running !! with anything other than sudo will append the argument to your most recent command
-# To add another command to prepend list remove the # on line 10 and put the command in the quotes. Repeat as needed
-function !!;
-  set var (history | head -n 1)
-  if test $argv
-    if test $argv = "sudo"        #; or "any other command you want to prepend"
-        eval $argv $var
-    else
-        eval $var $argv
-    end
-    else
-        eval $var
-  end
-end
-
 # Aliases
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias code='code-insiders'
-alias standing-setup='sh /home/lucas/.screenlayout/standing-setup.sh'
-alias sitting-setup='sh /home/lucas/.screenlayout/sitting-setup.sh'
-alias laptop-only='sh /home/lucas/.screenlayout/laptop-only.sh'
+alias standing-setup='sh /home/lucas/.config/scripts/standing-setup.sh'
+alias sitting-setup='sh /home/lucas/.config/scripts/sitting-setup.sh'
+alias laptop-only='sh /home/lucas/.config/scripts/laptop-only.sh'
 alias pdf='mupdf'
 alias hist='history --show-time="%d/%m/%Y %T " | less'
 alias conda-activate='eval ~/anaconda3/bin/conda "shell.fish" "hook" $argv | source'
@@ -53,11 +35,8 @@ set fish_color_param '#0070a3'
 set __fish_git_prompt_show_informative_status true
 set __fish_git_prompt_showcolorhints true
 set __fish_git_prompt_char_dirtystate '*'
-
 set -gx EDITOR nvim
-set -gx JAVA_HOME /usr/lib/jvm/java-11-openjdk
-set -gx MVN_HOME /opt/apache-maven-3.9.6/bin
-set -gx MAVEN_HOME /opt/apache-maven-3.9.6/bin
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 picom &
 # setup the screen layout before the xhost command, otherwise xrandr will be weird
-sh ~/.screenlayout/standing-setup.sh
+sh ~/.config/scripts/standing-setup.sh
 # allow lucas to use X, I need this for the cron job that changes the wallpaper
 xhost +si:localuser:lucas &
 1password &
