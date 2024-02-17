c9369c4c464c33e74d02d36c5b5cc2628cdf99d4
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-02-17 15:03:41 +0100

    updates plugin configs

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,10 @@
-require('telescope').setup{
+local status_ok, telescope = pcall(require, 'telescope')
+if not status_ok then
+    print("Telescope failed to load")
+    return
+end
+
+telescope.setup{
 		defaults = {
 				-- Default configuration for telescope goes here:
 				path_display = { "truncate" },
@@ -26,6 +32,6 @@ require('telescope').setup{
 local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {}) -- requires ripgrep installed
--- nvim/lua/lucas/packer.lua
+++ nvim/lua/lucas/packer.lua
@@ -1,20 +1,39 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+        'git',
+        'clone',
+        '--depth',
+        '1',
+        'https://github.com/wbthomason/packer.nvim',
+        install_path
+    })
+    print "Installing packer, close and reopen neovim"
+end
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+    print("Packer failed to load")
+    return
+end
+
+return packer.startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
 	-- Simple plugins can be specified as strings
+    use 'AlexvZyl/nordic.nvim' -- Nord theme
     use 'windwp/nvim-autopairs' -- Auto-pairing for quotes, parens, brackets, etc.
 	use 'nvim-lua/plenary.nvim' -- don't forget to add this one if you don't have it yet!
     use 'numToStr/Comment.nvim' -- toggle comments
     use 'nvim-tree/nvim-tree.lua' -- file explorer
     use 'lewis6991/gitsigns.nvim' -- git signs
     use 'nvim-lualine/lualine.nvim' -- status line
-	use 'AlexvZyl/nordic.nvim' -- Nord theme
 	use 'mbbill/undotree' -- undo tree
 	use 'tpope/vim-fugitive' -- git commands
     use 'windwp/nvim-ts-autotag' -- auto close html tags
@@ -38,7 +57,6 @@ return require('packer').startup(function(use)
 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	--:	requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
 	use {
@@ -61,7 +79,9 @@ return require('packer').startup(function(use)
 			{'neovim/nvim-lspconfig'},
 			-- Autocompletion
 			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-path'},
 			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-buffer'},
 			{'L3MON4D3/LuaSnip'},
 		}
 	}
