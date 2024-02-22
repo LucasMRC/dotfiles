e1ed6c62ca1329f02460ab2a9fde28d0b4fc694c
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-02-22 10:26:35 +0100

    adds diagnostics on leader key

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,12 +2,19 @@ local lsp = require('lsp-zero')
 
 lsp.preset("recommended")
 
+-- Disables inline diagnostics 
+vim.diagnostic.config({
+  virtual_text = false
+})
+
 lsp.on_attach(function(_, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
     local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    -- Adds diagnostics on space key
+    vim.keymap.set('n', '<leader>', function() vim.diagnostic.open_float(nil, { focus = false }) end, opts)
 
     lsp.default_keymaps({
         buffer = bufnr,
