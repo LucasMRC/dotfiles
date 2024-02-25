9f815ed9ce38b28f4ff513a1028d71967db0b499
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-02-25 08:47:03 +0100

    fixes indentation

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -14,15 +14,15 @@ vim.diagnostic.config({
 })
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-vim.lsp.diagnostic.on_publish_diagnostics, {
-    update_in_insert = false,
-})
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    { update_in_insert = false }
+)
 --
 
 -- Function signature
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-vim.lsp.handlers.signature_help,
-    {border = 'rounded'}
+    vim.lsp.handlers.signature_help,
+    { border = 'rounded' }
 )
 --
 
