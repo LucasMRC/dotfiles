5989d09c8b49546eca550d5d09e807d75f5f93f1
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-03-11 11:22:02 +0100

    makes HDMI-1 the primary screen

--- scripts/standing-setup.sh
+++ scripts/standing-setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x576 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1366x0 --rotate left --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+xrandr --output eDP-1 --mode 1366x768 --pos 0x576 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1366x0 --rotate left --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
 feh --bg-max --randomize ~/.desktop/
+
