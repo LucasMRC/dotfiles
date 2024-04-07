28efaf35d0155878f14dff367c049525462d61e9
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-04-07 11:42:01 +0200

    nvim: fixes nvim lua completions

--- nvim/lua/lucas/plugins/cmp.lua
+++ nvim/lua/lucas/plugins/cmp.lua
@@ -1,120 +1,123 @@
 return { -- Autocompletion
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				return "make install_jsregexp"
-			end)(),
-		},
-		"saadparwaiz1/cmp_luasnip",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        -- Snippet Engine & its associated nvim-cmp source
+        {
+            "L3MON4D3/LuaSnip",
+            build = (function()
+                return "make install_jsregexp"
+            end)(),
+        },
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-nvim-lua",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
-		luasnip.config.setup({})
+        luasnip.config.setup({})
 
-		local kind_icons = {
-			Text = "󰉿",
-			Method = "󰆧",
-			Function = "󰊕",
-			Constructor = "",
-			Field = " ",
-			Variable = "󰀫",
-			Class = "󰠱",
-			Interface = "",
-			Module = "",
-			Property = "󰜢",
-			Unit = "󰑭",
-			Value = "󰎠",
-			Enum = "",
-			Keyword = "󰌋",
-			Snippet = "",
-			Color = "󰏘",
-			File = "󰈙",
-			Reference = "",
-			Folder = "󰉋",
-			EnumMember = "",
-			Constant = "󰏿",
-			Struct = "",
-			Event = "",
-			Operator = "󰆕",
-			TypeParameter = " ",
-			Misc = " ",
-		}
+        local kind_icons = {
+            Text = "󰉿",
+            Method = "󰆧",
+            Function = "󰊕",
+            Constructor = "",
+            Field = " ",
+            Variable = "󰀫",
+            Class = "󰠱",
+            Interface = "",
+            Module = "",
+            Property = "󰜢",
+            Unit = "󰑭",
+            Value = "󰎠",
+            Enum = "",
+            Keyword = "󰌋",
+            Snippet = "",
+            Color = "󰏘",
+            File = "󰈙",
+            Reference = "",
+            Folder = "󰉋",
+            EnumMember = "",
+            Constant = "󰏿",
+            Struct = "",
+            Event = "",
+            Operator = "󰆕",
+            TypeParameter = " ",
+            Misc = " ",
+        }
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			completion = { completeopt = "menu,menuone,noinsert" },
-			mapping = cmp.mapping.preset.insert({
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete({}),
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
-			}),
-			formatting = {
-				fields = { "kind", "abbr", "menu" },
-				format = function(entry, vim_item)
-					-- Kind icons
-					vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-					vim_item.menu = ({
-						nvim_lsp = "[LSP]",
-						luasnip = "[Snippet]",
-						buffer = "[File]",
-						path = "[Path]",
-						["vim-dadbod-completion"] = "[DB]",
-					})[entry.source.name]
-					return vim_item
-				end,
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-			},
-			experimental = {
-				ghost_text = false,
-			},
-			view = {
-				docs = {
-					auto_open = true,
-				},
-			},
-			matching = {
-				disallow_fuzzy_matching = false,
-			},
-		})
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            completion = { completeopt = "menu,menuone,noinsert" },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete({}),
+                ["<C-l>"] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    end
+                end, { "i", "s" }),
+                ["<C-h>"] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    end
+                end, { "i", "s" }),
+            }),
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = function(entry, vim_item)
+                    -- Kind icons
+                    vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+                    vim_item.menu = ({
+                        nvim_lsp = "[LSP]",
+                        luasnip = "[Snippet]",
+                        buffer = "[File]",
+                        path = "[Path]",
+                        nvim_lua = "[NVIM]",
+                        ["vim-dadbod-completion"] = "[DB]",
+                    })[entry.source.name]
+                    return vim_item
+                end,
+            },
+            sources = {
+                { name = "nvim_lua" },
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
+            },
+            experimental = {
+                ghost_text = false,
+            },
+            view = {
+                docs = {
+                    auto_open = true,
+                },
+            },
+            matching = {
+                disallow_fuzzy_matching = false,
+            },
+        })
 
-		local autocomplete_group = vim.api.nvim_create_augroup("db-autocomplete", { clear = true })
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "sql", "mysql", "plsql" },
-			callback = function()
-				cmp.setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
-			end,
-			group = autocomplete_group,
-		})
-	end,
+        local autocomplete_group = vim.api.nvim_create_augroup("db-autocomplete", { clear = true })
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "sql", "mysql", "plsql" },
+            callback = function()
+                cmp.setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+            end,
+            group = autocomplete_group,
+        })
+    end,
 }
