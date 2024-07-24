eab3aff081bedae5f2eebcab8f36fcab64651d4b
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-07-24 07:07:18 +0200

    gfn: prevents window from closing when ending process

--- scripts/gfn.sh
+++ scripts/gfn.sh
@@ -4,22 +4,28 @@ DOTFILES_DIR=/home/lucas/.dotfiles/
 WEBSITE_DIR=/home/lucas/Repositories/grafana/website.git/
 
 # Website
-tmux new -d -s "Neovim" -n "Website" -c $WEBSITE_DIR "nvim ."
+tmux new -d -s "Neovim" -n "Website" -c $WEBSITE_DIR
+tmux send-keys -t "Neovim:Website" "invim ." Enter
 # Dotfiles
-tmux neww -t "Neovim:2" -n "Dotfiles" -c $DOTFILES_DIR "nvim ."
+tmux neww -t "Neovim:2" -n "Dotfiles" -c $DOTFILES_DIR
+tmux send-keys -t "Neovim:Dotfiles" "invim ." Enter
 # Obsidian
-tmux neww -t "Neovim:3" -n "Notes" -c $DOTFILES_DIR "nvim ."
+tmux neww -t "Neovim:3" -n "Notes" -c $DOTFILES_DIR
+tmux send-keys -t "Neovim:Notes" "invim ." Enter
 tmux send-keys -t "Neovim:Notes" Space no # open obsidian telescope extension
 
 # Tasks
 tmux new -d -s "Terminal" -n "Tasks" -c $WEBSITE_DIR
 # Monitoring
-tmux neww -t "Terminal:2" -n "HTOP" -c $DOTFILES_DIR "htop"
+tmux neww -t "Terminal:2" -n "HTOP" -c $DOTFILES_DIR
+tmux send-keys -t "Terminal:HTOP" "ihtop" Enter
 # Music player
-tmux neww -t "Terminal:3" -n "Music" -c $DOTFILES_DIR "spotify_player"
+tmux neww -t "Terminal:3" -n "Music" -c $DOTFILES_DIR
+tmux send-keys -t "Terminal:Music" "ispotify_player" Enter
 tmux send-keys -t "Terminal:Music" gs # go to global search
 # Github
-tmux neww -t "Terminal:4" -n "Github" -c $WEBSITE_DIR "gh dash"
+tmux neww -t "Terminal:4" -n "Github" -c $WEBSITE_DIR
+tmux send-keys -t "Terminal:Github" "igh dash" Enter
 
 # Select main windows
 tmux selectw -t "Terminal:Tasks"
