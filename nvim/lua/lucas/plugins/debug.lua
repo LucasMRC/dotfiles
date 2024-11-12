dcd1fc8e3e4f2d0da32346aa105f9523f996fa68
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-11-12 09:25:49 +0100

    nvim: removes unnecessary panes on dap

--- nvim/lua/lucas/plugins/debug.lua
+++ nvim/lua/lucas/plugins/debug.lua
@@ -13,7 +13,33 @@ return {
 			require('nvim-dap-virtual-text').setup({})
 			local dap_ui = require 'dapui'
 
-			dap_ui.setup()
+			dap_ui.setup({
+				controls = {
+					enabled = false
+				},
+				layouts = {
+					{
+						-- You can change the order of elements in the sidebar
+						elements = {
+							-- Provide IDs as strings or tables with "id" and "size" keys
+							{
+								id = "scopes",
+								size = 0.75, -- Can be float or integer > 1
+							},
+							{ id = "watches", size = 0.25 },
+						},
+						size = 40,
+						position = "left", -- Can be "left" or "right"
+					},
+					{
+						elements = {
+							"repl",
+						},
+						size = 10,
+						position = "bottom", -- Can be "bottom" or "top"
+					},
+				},
+			})
 
 			dap.listeners.before.attach.dap_ui_config = function()
 				dap_ui.open()
@@ -53,11 +79,10 @@ return {
 			set("n", "<leader>ds", ":DapStepOver<CR>", "[D]ebug [S]tep Over")
 			set("n", "<leader>di", ":DapStepInto<CR>", "[D]ebug Step [I]nto")
 			set("n", "<leader>do", ":DapStepOut<CR>", "[D]ebug Step [O]ut")
+			set("n", "<leader>dS", ":DapStop<CR>", "[D]ebug [S]top")
 			set("n", "<leader>dt", ":DapToggleBreakpoint<CR>", "[D]ebug [T]oggle Breakpoint")
-			set("n", "<leader>dC", ":lua require'dap'.set_breakpoint(vim.fn.input(('Breakpoint condition: ')))<CR>",
-				"[D]ebug [C]onditional")
-			set("n", "<leader>dl", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))",
-				"[D]ebug [L]og point")
+			set("n", "<leader>dC", ":lua require'dap'.set_breakpoint(vim.fn.input(('Breakpoint condition: ')))<CR>", "[D]ebug [C]onditional")
+			set("n", "<leader>dl", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))", "[D]ebug [L]og point")
 			set("n", "<leader>dr", function() dap_ui.open({ reset = true }) end, "[D]ebug ui [R]eset")
 			set("n", "<leader>du", dap_ui.toggle, "[D]ebug [U]i")
 		end,
@@ -65,7 +90,7 @@ return {
 	{
 		"leoluz/nvim-dap-go",
 		keys = {
-			{ "<leader>DS", mode = "n", desc = "[D]ap [S]tart" },
+			{ "<leader>DS", "<cmd>echo 'DAP Started'<CR>", desc = "[D]ap [S]tart", silent = true },
 		},
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
@@ -75,26 +100,27 @@ return {
 			local dap_go = require 'dap-go'
 			local find_cmd = vim.system({ "find", ".", "-type", "f", "-name", "main.go" }):wait()
 			local file_path = find_cmd.stdout:gsub("^%.", ""):gsub("/main.go\n", "")
+			local working_dir = vim.fn.getcwd()
 			local dap = require 'dap'
 
 			dap_go.setup({
 				dap_configurations = {
 					{
 						type = "go",
-						name = "DDeb",
+						name = "Debug",
 						request = "launch",
 						logOutput = "dap",
 						showLog = true,
-						program = "${workspaceFolder}" .. file_path,
+						program = working_dir .. file_path,
 					},
 					{
 						type = "go",
-						name = "DDeb with args",
+						name = "Debug with args",
 						request = "launch",
 						logOutput = "dap",
 						showLog = true,
 						args = require("dap-go").get_arguments,
-						program = "${workspaceFolder}" .. file_path,
+						program = working_dir .. file_path,
 					},
 				}
 			})
