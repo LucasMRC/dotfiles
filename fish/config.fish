8e5bf90d18b4f2cf6376c80608e62a19d5683807
LucasMRC <lucas.reynacordoba22@gmail.com>
2025-02-07 11:25:30 +0100

    fish: updates prompt

--- fish/config.fish
+++ fish/config.fish
@@ -32,6 +32,38 @@ if status is-login
         exec startx -- -keeptty
     end
 end
+
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+    set -q fish_color_status
+    or set -g fish_color_status red
+
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l suffix '>'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix '#'
+    end
+
+    # Write pipestatus
+    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
+    end
+    set __fish_prompt_status_generation $status_generation
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color $bold_flag $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+    echo -n -s ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status \n "~" $suffix " "
+end
     
 # Aliases
 alias ls='ls --color=auto'
@@ -49,6 +81,7 @@ alias tmux-launcher='sh ~/.config/scripts/tmux-launcher.sh'
 alias m='man -k . | fzf --preview "echo {} | awk \'{ print $1 }\' | xargs man" --preview-window "right,wrap" | xargs man'
 alias f='fzf --preview "cat {}" --preview-window "right,wrap" | xargs nvim'
 alias t='sh ~/.config/scripts/tmux-launcher.sh'
+alias vpn='sh ~/.config/scripts/vpn.sh'
 
 # Set up arandr layout aliases
 set SCREENS (ls ~/.screenlayout/ | sed 's/.sh//g')
@@ -95,3 +128,6 @@ end
 
 # Kitty
 set -gx KITTY_CONFIG_DIRECTORY "/home/lucas/.config/kitty"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/lucas/.local/bin/google-cloud-sdk/path.fish.inc' ]; . '/home/lucas/.local/bin/google-cloud-sdk/path.fish.inc'; end
