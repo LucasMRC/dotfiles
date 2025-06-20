da1c7b2c372561950fae85270558cd87e404e8ef
LucasMRC <lucas.reynacordoba22@gmail.com>
2025-06-20 07:48:59 +0200

    scripts: improves bare repos

--- scripts/git/add-worktree.sh
+++ scripts/git/add-worktree.sh
@@ -1,25 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -ef -o pipefail
+
+# if not in a git repository, this will fail and exit
+(git rev-parse)
+
+function usage {
+	cat << EOF
+Create a new worktree on a bare repository.
+
+Usage:
+	$0 <BRANCH> [ORIGIN]
+Examples:
+	$0 new-feature
+	$0 new-feature develop
+EOF
+}
+
+if [[ $# -ne 1 && $# -ne 2 ]]; then
+	usage
+	exit 2
+fi
 
 BRANCH=$1
 ORIGIN=$2
 CURR_D=$(pwd)
 
-if [[ "$CURR_D" != *.git ]]; then
-    echo "You have to be in the root directory of a git repository"
-    exit 1
+if [[ -z "$BRANCH" ]]; then
+	echo "You have to specify a branch name"
+	exit 1
 fi
 
-if [ -z $BRANCH ]; then
-    echo "You have to specify a branch name"
-    exit 1
-fi
+# Go to bare repo root directory
+cd $(git rev-parse --git-common-dir)
 
 echo "Creating new worktree..."
 
-if [ -z $ORIGIN ]; then
-    MAIN=$(git branch | grep -E '(main|master)' | gawk 'match($0, /master|main/) {print $2}')
-    ORIGIN="origin/$MAIN"
-	git fetch origin $MAIN
+if [[ -z "$ORIGIN" ]]; then
+	MAIN=$(git branch | grep -E '(main|master)' | gawk 'match($0, /master|main/) {print $2}')
+	ORIGIN="origin/$MAIN"
+	git fetch origin "$MAIN"
 fi
 
-git worktree add -b $BRANCH $BRANCH $ORIGIN
+git worktree add -b "$BRANCH" "$BRANCH" "$ORIGIN"
--- scripts/git/bare.sh
+++ scripts/git/bare.sh
@@ -1,11 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -ef -o pipefail
+
+function usage {
+	cat << EOF
+Clone a bare repository.
+
+Usage:
+	$0 <REPOSITORY> [DIRECTORY]
+Examples:
+	$0 git@github.com:grafana/website.git
+EOF
+}
+
+if [[ $# -ne 1 && $# -ne 2 ]]; then
+	usage
+
+	exit 2
+fi
+
+DIR=$2
+
+if [ -z $DIR ]; then
+	DIR=$(basename $1)
+fi
 
 echo "Cloning bare repository"
-git clone --bare "$1" # --depth=1
-DIR=$2
-if [ -z $DIR ]; then
-    DIR=$(sed 's/.*\/\(.*\.git\)/\1/' <<< $1)
-fi
+git clone --bare "$1" "$DIR"
+
 echo "Adjusting origin fetch locations..."
-cd $DIR && git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+(cd $DIR && git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*")
 echo "Success."
