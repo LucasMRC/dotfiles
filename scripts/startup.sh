4b447d6b2656f1ed36e28b24ad1f9bbb308ca758
LucasMRC <lucas.reynacordoba22@gmail.com>
2025-08-07 11:24:06 +0200

    adds gammastep

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -8,4 +8,4 @@ setxkbmap -layout us -variant intl # Keyboard layout
 gsettings set org.gtk.Settings.FileChooser window-size '(800,600)' # Set default file chooser windows size
 v4l2-ctl -d /dev/video0 --set-ctrl brightness=100 # Set camera brightness
 v4l2-ctl -d /dev/video0 --set-ctrl contrast=25 # Set camera contrast
-systemctl --user start spotifyd # For some reason fails when started as enabled service
+systemctl --user start gammastep
